2ed0a71c4a45c8eaa62d7a09542e2578c229278d
Eugene Pranoto <eugenepranoto96@gmail.com>
2023-08-18 18:37:12 +0700

    remove edit  function on favorite

--- src/Modules/Favorite/index.tsx
+++ src/Modules/Favorite/index.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import Box from '../../Components/Box';
 import Text from '../../Components/Text';
 import Loading from '../../Components/Loading';
-import Modal from '../../Components/Modal';
 
 import { Gap } from '../../Constants/Gap';
 import { Margin } from '../../Constants/Margin';
@@ -12,23 +11,17 @@ import { Margin } from '../../Constants/Margin';
 import useFavoriteLogic from './logic';
 import { Color } from '../../Constants/Color';
 
-const ContactForm = lazy(() => import('../Contact/Form'));
 const ContactCard = lazy(() => import('../Contact/Components/Card'));
 
 export default function Favorite(): JSX.Element {
     const { t } = useTranslation('common');
 
-    const { selectedContact, showModalEdit, favoritesData, loadingFavorite, handleOpenContactDetail, setShowModalEdit } = useFavoriteLogic();
+    const { favoritesData, loadingFavorite } = useFavoriteLogic();
     return (
         <Box direction="column" alignItems="center" rowGap={Gap.Small} style={{ padding: `${Margin.Medium} 0` }}>
-            <Modal title={selectedContact?.first_name} show={showModalEdit} handleClose={() => setShowModalEdit(false)}>
-                <Suspense fallback={<Loading />}>
-                    <ContactForm values={selectedContact} onSubmitCb={() => setShowModalEdit(false)}/>
-                </Suspense>
-            </Modal>
             <Suspense fallback={<Loading />}>
                 {favoritesData?.favorites?.map(el => (
-                    <ContactCard key={el.id} data={el} onClick={handleOpenContactDetail} isFavorite/>
+                    <ContactCard key={el.id} data={el} onClick={() => {}} isFavorite/>
                 ))}
             </Suspense>
             {!favoritesData?.favorites?.length && <Text fontSize="small" color={Color.Secondary}>{t('noData')}</Text>}
